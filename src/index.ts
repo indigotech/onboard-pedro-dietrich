98cac278b1de3852d1dd07ccadf82c80e90cecb8
pedro-m-dietrich <pedro.martins@taqtile.com.br>
2024-09-04 12:27:55 -0300

    Create mutation for adding a new user
    The mutation receives a user input data, creates a user from that by
    adding a random ID, saves it in a list of users (simulating a database)
    and returns the user (without the password).

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,55 @@ const typeDefs = `#graphql
   type Query {
     hello: String!
   }
+
+  input UserInput {
+    name: String!
+    email: String!
+    password: String!
+    birthDate: String!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    birthDate: String!
+  }
+
+  type Mutation {
+    createUser(user: UserInput!): User
+  }
 `;
+
+interface UserInput {
+  name: string;
+  email: string;
+  password: string;
+  birthDate: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  birthDate: string;
+}
+
+const users: User[] = [];
 
 const resolvers = {
   Query: {
     hello: () => 'Hello World!',
+  },
+  Mutation: {
+    createUser: (_, args: { user: UserInput }): User => {
+      const id = Math.floor(Math.random() * 100000);
+
+      const user = { ...args.user, id: id };
+      users.push(user);
+
+      return user;
+    },
   },
 };
 
