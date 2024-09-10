5d1f6af61c958b1d5d9c5a2d57cc9614f0d3d7d3
pedro-m-dietrich <pedro.martins@taqtile.com.br>
2024-09-10 11:41:58 -0300

    Add testing for fetching user query
    Three test cases were added for the `user` query:
    - Successfully fetching an user;
    - Failing to fetch an user with an inexistent ID.
    - Failing to fetch an user with an invalid JWT.

--- test/server.ts
+++ test/server.ts
@@ -7,7 +7,7 @@ import { ApolloServer } from '@apollo/server';
 import { initializeDatabaseInstance, startServer, prisma, DatabaseUserData } from '../src/server.js';
 import { User, UserInput } from '../src/typedefs.js';
 
-describe('Onboard server API', function () {
+describe('User API', function () {
   let server: ApolloServer;
   let url: string;
 
@@ -205,6 +205,125 @@ describe('Onboard server API', function () {
             },
             locations: [{ column: 9, line: 3 }],
             path: ['createUser'],
+          },
+        ],
+      });
+    });
+  });
+
+  describe('#User query', async function () {
+    const userData: UserInput = {
+      name: 'Test User',
+      email: 'test@user.com',
+      password: 'password123',
+      birthDate: '2000-01-01',
+    };
+
+    beforeEach(function () {
+      axios.defaults.headers.authorization = jwt.sign({ userId: 1 }, process.env.TOKEN_KEY, { expiresIn: '30m' });
+    });
+
+    afterEach(async function () {
+      await prisma.user.deleteMany({});
+    });
+
+    const getUserQuery = `#graphql
+      query GetUserQuery($userId: GetUserInput!) {
+        user(userId: $userId) {
+          id
+          name
+          email
+          birthDate
+        }
+      }
+    `;
+
+    it('should fetch the correct user by the ID', async function () {
+      const dbUser = await prisma.user.create({
+        data: {
+          name: userData.name,
+          email: userData.email,
+          password: userData.password,
+          birthDate: new Date(userData.birthDate),
+        },
+      });
+
+      const userQuery = {
+        operationName: 'GetUserQuery',
+        query: getUserQuery,
+        variables: { userId: { id: dbUser.id } },
+      };
+
+      const response = await axios.post(url, userQuery);
+      const foundUser: User = response.data.data.user;
+
+      expect(foundUser).to.be.deep.eq({
+        id: dbUser.id.toString(),
+        name: dbUser.name,
+        email: dbUser.email,
+        birthDate: new Date(dbUser.birthDate).getTime().toString(),
+      });
+    });
+
+    it('should fail to find user with inexistent ID', async function () {
+      const userQuery = {
+        operationName: 'GetUserQuery',
+        query: getUserQuery,
+        variables: { userId: { id: 123456789 } },
+      };
+
+      const response = await axios.post(url, userQuery);
+
+      expect(response.data).to.be.deep.eq({
+        data: {
+          user: null,
+        },
+        errors: [
+          {
+            message: 'User does not exist.',
+            extensions: {
+              code: 'INTERNAL_SERVER_ERROR',
+              additionalInfo: 'No user with the specified ID could be found.',
+            },
+            locations: [{ column: 9, line: 3 }],
+            path: ['user'],
+          },
+        ],
+      });
+    });
+
+    it('should fail to fetch user when unauthenticated', async function () {
+      axios.defaults.headers.authorization = '';
+      const dbUser = await prisma.user.create({
+        data: {
+          name: userData.name,
+          email: userData.email,
+          password: userData.password,
+          birthDate: new Date(userData.birthDate),
+        },
+      });
+
+      const userQuery = {
+        operationName: 'GetUserQuery',
+        query: getUserQuery,
+        variables: { userId: { id: dbUser.id } },
+      };
+
+      const response = await axios.post(url, userQuery);
+
+      expect(response.data).to.be.deep.eq({
+        data: {
+          user: null,
+        },
+        errors: [
+          {
+            message: 'Unauthenticated user.',
+            extensions: {
+              code: 'INTERNAL_SERVER_ERROR',
+              additionalInfo: 'The JWT is either missing or invalid.',
+            },
+            locations: [{ column: 9, line: 3 }],
+            path: ['user'],
           },
         ],
       });
