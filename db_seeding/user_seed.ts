72a29a0428c48453e45d2db07507e30e8bb7298d
pedro-m-dietrich <pedro.martins@taqtile.com.br>
2024-09-11 10:20:37 -0300

    Update seeding to create users in a single query
    Intead of using `upsert()` multiple times in a loop, the users are
    stored in an array to then be inserted in the database using
    `createMany()` with the `skipDuplicates` option enabled.

--- db_seeding/user_seed.ts
+++ db_seeding/user_seed.ts
@@ -5,21 +5,20 @@ initializeDatabaseInstance();
 async function main() {
   console.log('Seeding database users...');
 
+  const users = [];
   for (let i = 1; i <= 50; i++) {
     const name: string = 'User ' + i;
     const email: string = 'user' + i + '@seeded.com';
 
-    await prisma.user.upsert({
-      where: { email: email },
-      update: {},
-      create: {
-        name: name,
-        email: email,
-        password: 'password123',
-        birthDate: new Date('2000-01-01'),
-      },
+    users.push({
+      name: name,
+      email: email,
+      password: 'password123',
+      birthDate: new Date('2000-01-01'),
     });
   }
+
+  await prisma.user.createMany({ data: users, skipDuplicates: true });
 
   console.log('Database seeding done.');
 }
