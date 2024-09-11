f9162a275c192218c11908c39718641d3178aaec
pedro-m-dietrich <pedro.martins@taqtile.com.br>
2024-09-11 12:51:57 -0300

    Create tests for `users` query
    The tests verify if the `users` query for default values for `userLimit`
    and `offset`, for specified values and if unauthenticated users fail the
    request.

--- test/server.ts
+++ test/server.ts
@@ -329,4 +329,142 @@ describe('User API', function () {
       });
     });
   });
+
+  describe('#Users query', async function () {
+    let dbUsers: DatabaseUserData[];
+
+    beforeEach(async function () {
+      const usersInput = [];
+      for (let i = 1; i <= 50; i++) {
+        usersInput.push({
+          name: 'User ' + i.toString(),
+          email: 'user' + i.toString() + '@test.com',
+          password: 'password123',
+          birthDate: new Date('2001-02-03'),
+        });
+      }
+
+      dbUsers = await prisma.user.createManyAndReturn({ data: usersInput });
+      dbUsers.sort(function (userA, userB) {
+        if (userA.name < userB.name) {
+          return -1;
+        }
+        if (userA.name > userB.name) {
+          return 1;
+        }
+        return 0;
+      });
+
+      axios.defaults.headers.authorization = jwt.sign({ userId: 1 }, process.env.TOKEN_KEY, { expiresIn: '30m' });
+    });
+
+    afterEach(async function () {
+      await prisma.user.deleteMany({});
+    });
+
+    const getUserListQuery = `#graphql
+      query GetUserListQuery($usersInput: UserListInput) {
+        users(usersInput: $usersInput) {
+          users {
+            id
+            name
+            email
+            birthDate
+          }
+          totalUsers
+          userCount
+          offset
+          lastPage
+        }
+      }
+    `;
+
+    it('should retrieve an user list with default values', async function () {
+      const usersQuery = {
+        operationName: 'GetUserListQuery',
+        query: getUserListQuery,
+        variables: null,
+      };
+
+      const expectedList = dbUsers
+        .map(({ id, name, email, birthDate }) => ({
+          id: id.toString(),
+          name,
+          email,
+          birthDate: new Date(birthDate).getTime().toString(),
+        }))
+        .slice(0, 10);
+
+      const response = await axios.post(url, usersQuery);
+
+      expect(response.data.data).to.be.deep.eq({
+        users: {
+          users: expectedList,
+          totalUsers: dbUsers.length,
+          userCount: expectedList.length,
+          offset: 0,
+          lastPage: false,
+        },
+      });
+    });
+
+    it('should retrieve an user list with specified values', async function () {
+      const amount = 20;
+      const offset = 40;
+
+      const usersQuery = {
+        operationName: 'GetUserListQuery',
+        query: getUserListQuery,
+        variables: { usersInput: { userLimit: amount, offset: offset } },
+      };
+
+      const expectedList = dbUsers
+        .map(({ id, name, email, birthDate }) => ({
+          id: id.toString(),
+          name,
+          email,
+          birthDate: new Date(birthDate).getTime().toString(),
+        }))
+        .slice(offset, offset + amount);
+
+      const response = await axios.post(url, usersQuery);
+
+      expect(response.data.data).to.be.deep.eq({
+        users: {
+          users: expectedList,
+          totalUsers: dbUsers.length,
+          userCount: expectedList.length,
+          offset: offset,
+          lastPage: true,
+        },
+      });
+    });
+
+    it('should fail to fetch user list when unauthenticated', async function () {
+      axios.defaults.headers.authorization = '';
+
+      const usersQuery = {
+        operationName: 'GetUserListQuery',
+        query: getUserListQuery,
+        variables: null,
+      };
+
+      const response = await axios.post(url, usersQuery);
+
+      expect(response.data).to.be.deep.eq({
+        data: null,
+        errors: [
+          {
+            message: 'Unauthenticated user.',
+            extensions: {
+              code: 'INTERNAL_SERVER_ERROR',
+              additionalInfo: 'The JWT is either missing or invalid.',
+            },
+            locations: [{ column: 9, line: 3 }],
+            path: ['users'],
+          },
+        ],
+      });
+    });
+  });
 });
